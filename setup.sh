62a7e22c18090fe8eaf16ce42e010cecb05c02f8
660324lgr <59671452+660324lgr@users.noreply.github.com>
2022-06-01 17:17:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-mkdir -p ~/.streamlit
+mkdir -p ~/.streamlit/
+echo "[general]  
+email = \"m.fuad.rafi@gmail.com\""  > ~/.streamlit/credentials.toml
 echo "[server]
 headless = true
 port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
+enableCORS = false"  >> ~/.streamlit/config.toml
